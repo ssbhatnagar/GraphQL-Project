0ef3d1ba8af3f5d8d2894b4af7bc75cc09f352be
Bhatnagar <2063038@cognizant.com>
2024-08-11 12:35:24 +0530

    feat: added query variables

--- schema.js
+++ schema.js
@@ -1,25 +1,25 @@
 export const typeDefs = `#graphql
-
-type Game{
+  type Game {
     id: ID!
     title: String!
-    platform: [String!]! 
-}
-type Review{
+    platform: [String!]!
+  }
+  type Review {
     id: ID!
     rating: Int!
     content: String!
-}
-type Author{
+  }
+  type Author {
     id: ID!
     name: String!
-    verfied: Boolean
-}
-
-type Query{
-    reviews: [Review]
+    verified: Boolean!
+  }
+  type Query {
     games: [Game]
+    game(id: ID!): Game
+    reviews: [Review]
+    review(id: ID!): Review
     authors: [Author]
-}
-
+    author(id:ID!): Author
+  }
 `
